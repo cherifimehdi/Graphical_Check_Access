00d1bac213315c44b93e5cb94e9c66b8d64a7463
Mehdi Cherifi <73710391+cherifimehdi@users.noreply.github.com>
2021-08-26 12:18:59 +0000

    Update graphical_check_access.py

--- graphical_check_access.py
+++ graphical_check_access.py
@@ -1,13 +1,9 @@
-# Author : Mehdi CHERIFI
-
 from flask import Flask, render_template
 from genie.testbed import load
-from diagrams.generic.network import Switch
 import shutil
 from diagrams.generic.os import Ubuntu
 from diagrams import Diagram, Cluster
 from diagrams.custom import Custom
-
 
 def Check_Access(testbed):
 
@@ -22,19 +18,17 @@ def Check_Access(testbed):
       except Exception as e:
          Device_Decision[str(device)]="No Access"
    #Topology Devices Access
-   Routers_Switchs=[]
-   Topology_Devices={}
-   for i,j in Device_Decision.items():
-      Topology_Devices['{0}'.format(i)] = j
+   Topology_Devices=[]
+
    with Diagram("Topology Access Result", show=False, filename="Topology_Result"):
       PC = Ubuntu("Admin")
       with Cluster("Network Entreprise"):
 
-         SW=Custom("Switch", "./static/Switch.png")
-         for k,l in Topology_Devices.items():
-            Dev=Custom(k+'\n'+ l, "./static/Router.jpg")
-            Routers_Switchs.append(Dev)
-         PC >> SW >> Routers_Switchs
+         SWITCH=Custom("Switch", "./static/Switch.png")
+         for k,l in Device_Decision.items():
+            Dev=Custom(k+'\n'+ l, "./static/"+testbed.devices[k].type+".jpg")
+            Topology_Devices.append(Dev)
+         PC >> SWITCH >> Topology_Devices
    shutil.move("Topology_Result.png", "static/Topology_Result.png")
    return Device_Decision
 
